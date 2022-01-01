59606f27ff932f6812d35cfc00bc9ba52b99de38
Jordan Werthman <jordanwerthman@gmail.com>
2022-01-01 01:31:08 -0800

    Make colorswatch utility output prettier
    Generates an HTML pages containing all available color pallete
    swatches (8-bit color, 4-bit color, 4-bit greyscale).

--- bin/colorswatch.cc
+++ bin/colorswatch.cc
@@ -1,37 +1,62 @@
 #include <fstream>
 #include <iostream>
 
+#include "absl/strings/str_format.h"
 #include "color_palette.h"
-#include "core/logging.h"
 
 const char* HTML_TEMPLATE_HEADER = R"(
 <head>
   <style>
-    .container {
-      width: 1024px;
-      height: 256px;
+    .header {
+      padding: 5px;
+      font-size: 30px;
+      font-weight: bold;
     }
     .swatch {
       display: inline-block;
       width: 32px;
       height: 32px;
+      outline: 1px solid;
     }
   </style>
 </head>
 <body>
-  <div class="container">
 )";
+
+const char* HTML_TEMPLATE_SECTION_HEADER = R"(
+  <div class="header">%s</div>
+  <div>
+)";
+
+const char* HTML_TEMPLATE_SWATCH = R"(
+    <div class="swatch", style="background-color: rgb(%d, %d, %d);"></div>
+)";
+
+const char* HTML_TEMPLATE_SECTION_FOOTER = "</div>";
+const char* HTML_TEMPLATE_FOOTER = "</body>";
+
+void GenerateSection(const std::string& name,
+                     std::tuple<int, int, int> (*colorAtIndex)(uint8_t),
+                     size_t count,
+                     std::ostream& output) {
+  output << absl::StrFormat(HTML_TEMPLATE_SECTION_HEADER, name);
+  for (int index = 0; index < count; ++index) {
+    auto color = colorAtIndex(index);
+    output << absl::StrFormat(HTML_TEMPLATE_SWATCH, std::get<0>(color),
+                              std::get<1>(color), std::get<2>(color));
+  }
+  output << HTML_TEMPLATE_SECTION_FOOTER;
+}
 
 int main() {
   std::ofstream output;
   output.open("/tmp/color_swatch.html", std::ios::out);
   output << HTML_TEMPLATE_HEADER;
-  for (int index = 0; index < 256; ++index) {
-    auto color = colorAtIndex(index);
-    output << "<div class='swatch' style='background-color: rgb("
-           << (int)std::get<0>(color) << ", " << (int)std::get<1>(color) << ", "
-           << (int)std::get<2>(color) << ");'></div>";
-  }
-  output << "</div></body>";
+
+  GenerateSection("4-Bit Greyscale", colorAtIndex4BitGreyscale, 16, output);
+  GenerateSection("4-Bit Color", colorAtIndex4Bit, 16, output);
+  GenerateSection("8-Bit Color", colorAtIndex, 256, output);
+
+  output << HTML_TEMPLATE_FOOTER;
   return 0;
 }
--- color_palette.h
+++ color_palette.h
@@ -38,25 +38,31 @@ std::tuple<int, int, int> colorAtIndex(uint8_t index) {
 }
 
 // https://en.wikipedia.org/wiki/List_of_software_palettes#Apple_Macintosh_default_16-color_palette
-std::tuple<int, int, int> colorAtIndex4Bit(uint8_t byte) {
+std::tuple<int, int, int> colorAtIndex4Bit(uint8_t nibble) {
   std::vector<std::tuple<int, int, int>> COLORS = {
-    {255, 255, 255},  // White
-    {255, 255, 0},    // Yellow
-    {255, 165, 0},    // Orange
-    {255, 0, 0},      // Red
-    {255, 0, 255},    // Magenta
-    {128, 0, 128},  // Purple
-    {0, 0, 255},      // Blue
-    {0, 255, 255},    // Cyan
-    {0, 255, 0},      // Green
-    {0, 100, 0},      // Dark Green
-    {128, 64, 0},     // Brown
-    {210, 180, 140},  // Tan
-    {192, 192, 192},  // Light Grey
-    {128, 128, 128},  // Medium Grey
-    {64, 64, 64},     // Dark Grey
-    {0, 0, 0},        // Black
+      {255, 255, 255},  // White
+      {255, 255, 0},    // Yellow
+      {255, 165, 0},    // Orange
+      {255, 0, 0},      // Red
+      {255, 0, 255},    // Magenta
+      {128, 0, 128},    // Purple
+      {0, 0, 255},      // Blue
+      {0, 255, 255},    // Cyan
+      {0, 255, 0},      // Green
+      {0, 100, 0},      // Dark Green
+      {128, 64, 0},     // Brown
+      {210, 180, 140},  // Tan
+      {192, 192, 192},  // Light Grey
+      {128, 128, 128},  // Medium Grey
+      {64, 64, 64},     // Dark Grey
+      {0, 0, 0},        // Black
   };
-  CHECK(byte < 16);
-  return COLORS[byte];
+  CHECK(nibble < 16);
+  return COLORS[nibble];
+}
+
+std::tuple<int, int, int> colorAtIndex4BitGreyscale(uint8_t nibble) {
+  CHECK(nibble < 16);
+  int value = 255 - (nibble * 17);
+  return {value, value, value};
 }
