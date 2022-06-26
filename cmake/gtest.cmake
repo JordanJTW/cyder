77ffb97beb9dcc7fdd4b2adeec3c66d8ba26c7b9
Jordan Werthman <jordanwerthman@gmail.com>
2022-06-25 23:37:31 -0700

    Add googletest to project
    Allows testing of targets. Added a new cmake/gtest.cmake which
    defines a macro making it easy to define a gtest executable and
    discover all available tests.
    
    Run tests using ctest:
    
    ctest --test-dir build/Debug

---
+++ cmake/gtest.cmake
@@ -0,0 +1,10 @@
+#! gtest: define and discover test targets in $target_name
+#
+# Expects a .cc file with the same name to be present
+macro(gtest target_name)
+  add_executable(${target_name} ${target_name}.cc)
+  target_link_libraries(${target_name} gtest_main)
+
+  include(GoogleTest)
+  gtest_discover_tests(${target_name})
+endmacro()
