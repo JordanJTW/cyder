153d7f9a377025998fb76dd2f04e9e6fd33177b6
Jordan Werthman <jordanwerthman@gmail.com>
2022-07-20 22:58:35 -0700

    Add generated system globals enum
    Generated from data from Mac Almanac II [1] with the following
    modifications to allow for parsing:
    
     * Make descriptions occupy a single line
     * Remove s/\//Or on Line 157
     * Remove ¥ from the start of Line 145 (MaskPtr)
     * Remove space in "HFS Globals" on Line 168
    
    [1] http://www.mac.linux-m68k.org/devel/macalmanac.php

---
+++ gen/global_names_gen.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+
+GLOBAL_NAMES_PATH = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), 'global_names.txt')
+
+GLOBAL_NAMES_ENTRY = re.compile(
+    r'^(?P<name>[\*\?\w\[\]]+)\s+(?P<address>[\d\w]{4})\s+(?P<size>byte|word|long|\d+|-)(?P<description>.+)')
+
+
+HEADER = """
+#pragma once
+
+#include <cstdint>
+
+"""
+
+
+def parse_args():
+    if len(sys.argv) != 2:
+        print('Output path required')
+        sys.exit(-1)
+
+    output = sys.argv[1]
+    print('Saving to:', output + '.{cc,h}')
+    cc_path = '{}.cc'.format(output)
+    h_path = '{}.h'.format(output)
+    return (cc_path, h_path)
+
+
+def main():
+    (cc_path, h_path) = parse_args()
+
+    address_to_metadata = {}
+    error_lines = []
+    with open(GLOBAL_NAMES_PATH, 'r') as f:
+        for count, line in enumerate(f.readlines()):
+            # Skip blank lines
+            if not line.strip():
+                continue
+
+            match = GLOBAL_NAMES_ENTRY.match(line)
+            if match := GLOBAL_NAMES_ENTRY.match(line):
+                name = match.group('name')
+                address = match.group('address')
+                size = match.group('size')
+                description = match.group('description')
+
+                if size == 'byte':
+                    size = 1
+                elif size == 'word':
+                    size = 2
+                elif size == 'long':
+                    size = 4
+
+                address = int(address, 16)
+
+                metadata = address_to_metadata.get(address, [])
+                metadata.append({'name': name, 'size': size,
+                                'description': description})
+                address_to_metadata[address] = metadata
+
+            else:
+                error_lines.append((count + 1, line))
+
+    if error_lines:
+        for (count, line) in error_lines:
+            print(f'Error line {count}: "{line.strip()}"')
+        sys.exit(-2)
+
+    # Write the .h
+    with open(h_path, 'w') as f:
+        f.write(HEADER)
+
+        f.write('enum GlobalVars {')
+        for address, metadata in address_to_metadata.items():
+            names = set((item['name'].removeprefix('*')
+                        for item in metadata if item['name'] != '[????]'))
+            for name in names:
+                f.write(f"\n  {name} = 0x{address:X},")
+        f.write('\n};')
+
+    # Write the .cc
+    with open(cc_path, 'w') as f:
+        f.write("")
+
+
+if __name__ == '__main__':
+    main()
