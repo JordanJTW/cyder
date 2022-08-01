a11dfb4d1a06772089e0e26ec1aa2fa2660263cd
Jordan Werthman <jordanwerthman@gmail.com>
2022-07-31 23:19:30 -0700

    gen: Improve CodeGen and add ReadType<> implementation
    Adds a method, ReadType<>, which will read the types out of memory
    performing the necessary endian-ness transforms.
    Adds missing #pragma once to header and missing includes
    Adds spaces to the pretty-print output and fixes potential issue with
    printing u8 (they are interpreted as chars and a value of 0 ends output)
    Removes the final comma in the pretty-print output
    Fixes outputting builtin types so that they always output the C type
    Fixes the Offset enum by making it an "enum class" so the members do
    not leak into the surrounding scope (default behavior for a C enum)

--- gen/typegen/codegen.py
+++ gen/typegen/codegen.py
@@ -4,6 +4,35 @@ from typing import List
 from parser import Expression
 
 
+_READTYPE_PROTOTYPE = \
+    "absl::StatusOr<{}> ReadType(const core::MemoryRegion& region, size_t ptr)"
+
+
+_HEADER_INCLUDES = [
+    '<cstdint>',
+    '<ostream>',
+    '"absl/status/statusor.h"',
+    '"core/memory_region.h"',
+]
+
+_SOURCE_INCLUDES = [
+    '"core/endian_helpers.h"',
+    '"core/status_helpers.h"',
+    '"emu/memory/memory_map.h"'
+]
+
+
+def _get_c_type(type):
+  builtin_types = {
+    'u8': 'uint8_t',
+    'u16': 'uint16_t',
+    'u32': 'uint32_t',
+    'i16': 'int16_t',
+    'i32': 'int32_t'
+  }
+  return builtin_types.get(type, type)
+
+
 class CodeGenerator:
   def __init__(self, expressions):
     self._type_expressions = [
@@ -41,18 +70,7 @@ class CodeGenerator:
 
   def _generate_type_decls(self, file):
     for expr in self._type_expressions:
-      def get_type():
-        builtin_types = {
-          'u8': 'uint8_t',
-          'u16': 'uint16_t',
-          'u32': 'uint32_t',
-          'i16': 'int16_t',
-          'i32': 'int32_t'
-        }
-        type = expr["type"]
-        return builtin_types.get(type, type)
-
-      self._write_type(file, expr['label'], get_type())
+      self._write_type(file, expr['label'], _get_c_type(expr['type']))
 
   def _write_type_offsets(self, file, members):
     file.write('  enum class Offsets {\n')
@@ -68,12 +86,23 @@ class CodeGenerator:
     file.write(f'struct {label} {{\n')
     self._write_type_offsets(file, members)
     for member in members:
-      file.write(f'  {member["type"]} {member["name"]};\n')
+      file.write(f'  {_get_c_type(member["type"])} {member["name"]};\n')
     file.write('};\n')
 
   def _generate_struct_decls(self, file):
     for expr in self._struct_expressions:
       self._write_struct(file, expr['label'], expr['members'])
+
+  def _write_read_type_declare(self, file, type):
+    file.write(f'{_READTYPE_PROTOTYPE.format(type)};\n')
+
+  def _generate_read_type_declare(self, file):
+    file.write('template <typename T>\n')
+    self._write_read_type_declare(file, 'T')
+
+    for expr in self._struct_expressions:
+      file.write('template<> ')
+      self._write_read_type_declare(file, expr['label'])
 
   def _write_struct_stream_declare(self, file, label):
     file.write(f'std::ostream& operator<<(std::ostream&, const {label}&);\n')
@@ -82,9 +111,14 @@ class CodeGenerator:
     for expr in self._struct_expressions:
       self._write_struct_stream_declare(file, expr['label'])
 
+  def _write_includes(self, file, includes):
+    for include in includes:
+      file.write(f'#include {include}\n')
+
   def _generate_header(self, output_path):
     with open(f'{output_path}.h', 'w') as header:
-      header.write('#include <cstdint>\n#include <ostream>\n')
+      header.write('#pragma once\n\n')
+      self._write_includes(header, _HEADER_INCLUDES)
       header.write('\n')
 
       self._generate_type_decls(header)
@@ -93,13 +127,36 @@ class CodeGenerator:
       self._generate_struct_decls(header)
       header.write('\n')
 
+      self._generate_read_type_declare(header)
+      header.write('\n')
+
       self._generate_struct_stream_declare(header)
+
+  def _write_read_type(self, file, label, members):
+    file.write(f'template<> {_READTYPE_PROTOTYPE.format(label)} {{\n')
+    file.write(f'  {label} obj;\n')
+    for member in members:
+      (name, type) = (member['name'], member['type'])
+      type = _get_c_type(type)
+
+      file.write(
+        f'  obj.{name} = betoh<{type}>(TRY(cyder::memory::kSystemMemory.Copy<{type}>(ptr + int({label}::Offsets::{name}))));\n')
+    file.write('   return obj;\n')
+    file.write('}\n')
+
+  def _generate_read_type(self, file):
+    for expr in self._struct_expressions:
+      self._write_read_type(file, expr['label'], expr['members'])
 
   def _write_struct_stream(self, file, label, members):
     file.write(
       f'std::ostream& operator<<(std::ostream& os, const {label}& obj) {{ return os')
-    for member in members:
-      file.write(f'<< "{member["name"]}:" << obj.{member["name"]}')
+    for index, member in enumerate(members):
+      line_end = ' << ", "' if index + 1 != len(members) else ''
+      # Cast value to an int to work around printing u8 and having them appear as char...
+      # Without this any u8 set to 0 ends up being interpreted as an \0 for a string.
+      file.write(
+        f' << "{member["name"]}: " << int(obj.{member["name"]}){line_end}')
     file.write('; }\n')
 
   def _generate_struct_stream(self, file):
@@ -108,7 +165,14 @@ class CodeGenerator:
 
   def _generate_source(self, output_path):
     with open(f'{output_path}.cc', 'w') as source:
-      source.write('#include "types.h"\n')
+      # FIXME: Use output path to determine the include
+      source.write('#include "generated_types.h"\n')
+      source.write('\n')
+
+      self._write_includes(source, _SOURCE_INCLUDES)
+      source.write('\n')
+
+      self._generate_read_type(source)
       source.write('\n')
 
       self._generate_struct_stream(source)
