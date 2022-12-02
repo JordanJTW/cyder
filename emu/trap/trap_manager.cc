e6dea772ce9cad6ec0641232cd42103c2c5aeee5
Jordan Werthman <jordanwerthman@gmail.com>
2022-12-01 20:59:04 -0800

    TRAP: Consistently return 'noErr' for no-op traps

--- emu/trap/trap_manager.cc
+++ emu/trap/trap_manager.cc
@@ -315,6 +315,7 @@ absl::Status TrapManager::DispatchNativeSystemTrap(uint16_t trap) {
     case Trap::MoreMasters: {
       // Memory manager currently assumes max heap already and
       // heap fragmentation is not a huge concern right now.
+      m68k_set_reg(M68K_REG_D0, /*noErr*/ 0);
       return absl::OkStatus();
     }
     // Link: http://0.0.0.0:8000/docs/mac/Memory/Memory-125.html
