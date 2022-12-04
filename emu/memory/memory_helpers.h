5f7ccb3d6681fb70c938932eefd379502510b81e
Jordan Werthman <jordanwerthman@gmail.com>
2022-12-03 21:22:42 -0800

    Move WithType<Type>() to memory_helpers.h for re-usability :P

---
+++ emu/memory/memory_helpers.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <functional>
+
+#include "absl/status/status.h"
+#include "emu/memory/memory_map.h"
+#include "gen/typegen/generated_types.tdef.h"
+
+namespace cyder {
+
+template <typename Type>
+absl::Status WithType(Ptr ptr, std::function<absl::Status(Type& type)> cb) {
+  auto type = TRY(ReadType<Type>(memory::kSystemMemory, ptr));
+  RETURN_IF_ERROR(cb(type));
+  return WriteType<Type>(type, memory::kSystemMemory, ptr);
+}
+
+}  // namespace cyder
